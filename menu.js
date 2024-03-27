653a1c2439916306b33c67fcd400cef39739f2c4
Davi Dias <davitdt206@gmail.com>
2024-03-26 23:47:28 -0300

    menu mobile

---
+++ menu.js
@@ -0,0 +1,13 @@
+let btn_Menu = document.getElementById('btn_Menu')
+let = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
+
+btn_Menu.addEventListener('click',()=>{
+    menu.classList.add('abrir-menu')
+})
+menu.addEventListener('click',()=>{
+    menu.classList.remove('abrir-menu')
+})
+menu.addEventListener('click',()=>{
+    menu.classList.remove('abrir-menu')
+})
