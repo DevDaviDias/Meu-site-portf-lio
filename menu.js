3d4548dec23676060b7862dedc3a3866027d870b
devdavidias <davitdt206@gmail.com>
2024-12-13 07:10:54 -0300

    clean

--- menu.js
+++ menu.js
@@ -18,32 +18,33 @@ document.querySelectorAll('nav a').forEach(anchor => {
     });
 });
 
-type="text/javascript">
-function translateToEnglish() {
-  var selectElement = document.querySelector(".goog-te-combo");
-  selectElement.value = "en"; // Define o valor do menu suspenso para inglês
-  selectElement.dispatchEvent(new Event('change')); // Dispara o evento de mudança para acionar a tradução
-}
+type = "text/javascript" >
+    function translateToEnglish() {
+        var selectElement = document.querySelector(".goog-te-combo");
+        selectElement.value = "en"; // Define o valor do menu suspenso para inglês
+        selectElement.dispatchEvent(new Event('change')); // Dispara o evento de mudança para acionar a tradução
+    }
 
 const menuDiv = document.getElementById('menu-mobile')
-const btnAnimar = document.getElementById('btn-menu') 
+const btnAnimar = document.getElementById('btn-menu')
 
-menuDiv.addEventListener('click',animarmenu)
+menuDiv.addEventListener('click', animarmenu)
 
-function animarmenu(){
+function animarmenu() {
     menuDiv.classList.toggle('abrir')
-    btnAnimar.classList.toggle('ativo')}
+    btnAnimar.classList.toggle('ativo')
+}
 
-  // Função para exibir a mensagem de sucesso
-  function exibirMensagem() {
+// Função para exibir a mensagem de sucesso
+function exibirMensagem() {
     document.getElementById("mensagem-enviada").style.display = "block";
 }
 
 //alert("Seu formulário foi enviado com sucesso
 
-document.getElementById("formulario").addEventListener("submit", function(event){
+document.getElementById("formulario").addEventListener("submit", function (event) {
     event.preventDefault(); // Evita que o formulário seja enviado normalmente
     alert("Seu formulário foi enviado com sucesso!"); // Exibe o alerta
     // Você pode adicionar aqui qualquer outra lógica que precise ser executada após o envio do formulário
     document.getElementById("formulario").reset(); // Limpa os campos do formulário após o envio
-  });
+});
